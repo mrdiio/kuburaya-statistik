dcec4951a2fbeb8655980dd9d513a99c0293a43c
Dio <dionatsir@gmail.com>
2023-05-31 23:37:06 +0700

    change navbar

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -55,6 +55,70 @@ export default function Landing() {
             </div>
           </div>
         </div>
+
+        <div className="col-md-4">
+          <div className="card p-2 rounded-4">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card content. Some quick example text to build
+                on the card title and make up the bulk of the card content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card p-2 rounded-4">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card content. Some quick example text to build
+                on the card title and make up the bulk of the card content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card p-2 rounded-4">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card content. Some quick example text to build
+                on the card title and make up the bulk of the card content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card p-2 rounded-4">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card content. Some quick example text to build
+                on the card title and make up the bulk of the card content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
