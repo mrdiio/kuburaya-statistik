219de766ca7be95b1695d80f33b9cc0ff66f1421
Dio <dionatsir@gmail.com>
2023-05-31 21:23:37 +0700

    add heroes

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,11 +1,45 @@
+import Heroes from '../Components/Heroes';
+
 export default function Landing() {
   return (
     <div className="vh-100">
-      <div className="container-xxl h-75">test</div>
+      <Heroes />
 
       <h3>Organisasi</h3>
 
-      <div className="row py-2">
+      <div className="row py-2 g-4">
+        <div className="col-md-4">
+          <div className="card p-2 rounded-4">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card content. Some quick example text to build
+                on the card title and make up the bulk of the card content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card p-2 rounded-4">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card content. Some quick example text to build
+                on the card title and make up the bulk of the card content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className="col-md-4">
           <div className="card p-2 rounded-4">
             <div className="card-body">
